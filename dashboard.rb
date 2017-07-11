d6948d2970257c9d0cc763e6e743f7ec87da5654
Sean Luckett <whitecrane007@gmail.com>
2017-07-11 09:24:31 -0600

    Search by terms, and return first 30 results from Dice
    The layout isn't what I want, yet. However, this scrapes data with custom search terms and displays results.

--- dashboard.rb
+++ dashboard.rb
@@ -1,12 +1,41 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require_relative 'vendor/job_scraper'
+require_relative 'vendor/google/sheets_job_storage'
 
 class ScraperDashboard < Sinatra::Application
+  enable :sessions
+
+# Avoid warning: http://stackoverflow.com/a/18047653/5113832
+  set :session_secret, '*&(^B234'
+
   configure :development do
     register Sinatra::Reloader
   end
 
+  set :views, File.expand_path('../views', __FILE__)
+
   get '/' do
-    'Hello world!'
+    erb :index
+  end
+
+  get '/results' do
+    scraper = JobScraper.new(
+      search_terms: session[:search_terms],
+      writer: SheetsJobStorage
+    )
+
+    erb :results, locals: { job_listings: scraper.listings }
+  end
+
+  post '/save-job-listings' do
+    # TODO: implement
+  end
+
+  post '/search-jobs' do
+    url_search_terms = params[:search_terms].gsub(/\s+/, '_')
+    session[:search_terms] = url_search_terms
+
+    redirect to('/results')
   end
 end
--- vendor/job_scraper.rb
+++ vendor/job_scraper.rb
@@ -2,18 +2,21 @@ require 'mechanize'
 require 'active_support/time'
 
 class JobScraper
-  DICE_SEARCH_URL = 'https://www.dice.com/jobs/jtype-Full%20Time-q-rails_full_stack_web-l-Denver%2C_CO-radius-10-jobs'.freeze
+  DICE_SEARCH_URL_BEGIN = 'https://www.dice.com/jobs/jtype-Full%20Time-q-'.freeze
+  DICE_SEARCH_URL_END = '-l-Denver%2C_CO-radius-10-jobs'.freeze
   DICE_JOB_CSS = '.complete-serp-result-div'.freeze
+  GOOGLE_SHEET_NAME = 'Dice'
 
   attr_reader :listings
 
-  def initialize(scraper: Mechanize.new, writer:)
-    @page = scraper.get DICE_SEARCH_URL
-    @listings = job_listings
+  def initialize(scraper: Mechanize.new, search_terms:, writer:)
+    @page = scraper.get "#{DICE_SEARCH_URL_BEGIN}#{search_terms}#{DICE_SEARCH_URL_END}"
+    @listings = parse_jobs
+    @writer = writer
   end
 
-  def parse
-    @listings.each_with_index.map do |job, i|
+  def parse_jobs
+    job_listings.each_with_index.map do |job, i|
       position_info = job.css("a#position#{i}").first
       title = position_info['title']
       link = position_info['href']
@@ -23,6 +26,7 @@ class JobScraper
       location = extract_location(job)
 
       date = extract_posting_date(job)
+
 
       JobListing.new(title, company_name, location,
                      link, company_id, job_id, date)
@@ -43,6 +47,8 @@ class JobScraper
     date_string = job.css('ul.details li.posted').first.content
     date_command_string = date_string.split(' ').join('.')
 
+    date_command_string = '10.minutes.ago' if date_command_string =~ /moments/
+
     begin
       date = eval(date_command_string)
     rescue StandardError => e
@@ -59,6 +65,10 @@ class JobScraper
     job_id = URI.unescape(path_parts.last)
 
     [company_id, job_id]
+  end
+
+  def write_data(job_data_array)
+    @writer.save_job GOOGLE_SHEET_NAME, job_data_array
   end
 end
 
